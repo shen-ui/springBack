ed6bba31073b17dd503bbf70800bbe2cfd64ca2d
shen-ui <peterhuiwang1995@gmail.com>
2023-10-02 02:26:58 -0400

    problem: need to add schema when connecting through JPA

--- src/main/java/api/victorxie/blog/Blog/Post.java
+++ src/main/java/api/victorxie/blog/Blog/Post.java
@@ -1,20 +1,34 @@
 package api.victorxie.blog.Blog;
 
+import jakarta.persistence.*;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import java.time.ZonedDateTime;
+@Getter
+@Entity
+@Table
 public class Post {
+    @jakarta.persistence.Id
+    @Id
+    @SequenceGenerator(
+            name="post_sequence",
+            sequenceName= "post_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator="post_sequence"
+    )
     private Long id;
     private String title;
     private String entry;
     private ZonedDateTime date;
+    public Post(){}
     public Post(Long id, String entry, String title){
         setId(id);
         setEntry(entry);
         setTitle(title);
         setDate();
-    }
-    public void setId(Long id){
-        this.id = id;
     }
     public void setTitle(String title){
         this.title = title;
@@ -22,10 +36,8 @@ public class Post {
     public void setEntry(String entry){
         this.entry = entry;
     }
-    public void setDate(){
-        this.date = ZonedDateTime.now();
-    }
-    public Long getId(){ return (this.id); }
+    public void setDate(){ this.date = ZonedDateTime.now(); }
+    public void setId(Long id) { this.id = id; }
     public String getEntry(){ return (this.entry); }
     public String getTitle(){ return (this.title); }
     public ZonedDateTime getDate(){ return (this.date); }
@@ -35,12 +47,12 @@ public class Post {
         if(!this.title.equals(title) && !this.entry.equals(entry)) setDate();
     }
     @Override
-    public String toString(){
-        return(
-            "ID: "    + getId()    + "\n" +
-            "Title: " + getTitle() + "\n" +
-            "Entry: " + getEntry() + "\n" +
-            "Date: "  + getDate()  + "\n"
+    public String toString() {
+        return (
+                "ID: " + getId() + "\n" +
+                        "Title: " + getTitle() + "\n" +
+                        "Entry: " + getEntry() + "\n" +
+                        "Date: " + getDate() + "\n"
         );
     }
 }
